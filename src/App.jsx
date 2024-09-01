2d4c52d9b0950dd8c7795382fd2453f2a3c43845
mukesh phulwani <mukeshphulwani66@gmail.com>
2024-09-01 11:42:13 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 
 function App() {
 
+  const h5text = ""
+
   return (
     <>
       <h1>Github Actions</h1>
       <h2>Complete course</h2>
+      <h5>{{h5text}}</h5>
     </>
   )
 }
