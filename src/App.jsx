2dd696b17abbcfc0156539a5672fdc0779068526
mukesh phulwani <mukeshphulwani66@gmail.com>
2024-09-13 21:19:41 +0530

    fixed test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 
 function App() {
 
-  const h5text = ""
+ 
 
   return (
     <>
       <h1>Github Actions</h1>
       <h2>Complete course test 2</h2>
-      <h5>{{h5text}}</h5>
+     <h5>sdasd</h5>
     </>
   )
 }
